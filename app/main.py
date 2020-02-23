e2fe02a17d398e61a7703dbd3de1838c27eadd2f
sunflower33a <tnguyen19@wooster.edu>
2020-02-22 20:11:02 -0500

    Add cors

--- app/main.py
+++ app/main.py
@@ -3,9 +3,21 @@ api.main.py
 """
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "https://pearlhacks2020-mauve.herokuapp.com/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def liveness():
