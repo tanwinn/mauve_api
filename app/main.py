cdcf50eba9287f2a5edcd5c6dfb92f142fb6d885
Angel Boyd <angeltana99@gmail.com>
2020-02-22 20:59:53 -0500

    Update to the main file

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ api.main.py
 """
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -19,6 +20,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class Blog(BaseModel): 
+    title: str
+    author: User
+    summary: str 
+
+    
+    
+app = FastAPI()
+
 @app.get("/")
 async def liveness():
     return {"status": "OK"}
+
+@app.post("/users") 
+async def create_user(user_profile: User):
+    return {"user" : user_profile}
+
+@app.get("/users/{emails}")
+async def get_user_email(email):
+    return {"email" : email}
+
+@app.post("/blogs")
+async def create_post(whole_post: Blog):
+    return {"post" : whole_post}
