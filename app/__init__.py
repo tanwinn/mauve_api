29788459ab1e71e2b9b90a87079653b1f46fa984
sunflower33a <tnguyen19@wooster.edu>
2020-02-23 00:19:47 -0500

    Add Mongo utils

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+"""
+FastApi Mauve Python app
+"""
+
+from app.mauve_db import *
--- app/main.py
+++ app/main.py
@@ -2,8 +2,15 @@
 api.main.py
 """
 
+import logging
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+import pymongo
+
+import app.mauve_db as mauve_db
+
+APP_LOGGER = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -19,6 +26,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.on_event("startup")
+async def startup():
+    APP_LOGGER.warning("Starting up app...")
+    mauve_db.client_factory()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    APP_LOGGER.warning("Shuting down app...")
+    mauve_db.shutdown_client()
+
 @app.get("/")
 async def liveness():
     return {"status": "OK"}
+
+@app.get("/db")
+async def test_db():
+    mauve_db.insert_collection("users", {"foo": "bar"})
+    result = mauve_db.count("users")
+    print(result)
+    return result
