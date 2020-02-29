c90d056b7eb4ff4ce772c2fad197790f7216001c
Thanh Nguyen <tnguyen19@wooster.edu>
2020-02-29 00:15:25 -0500

    Add Testing framework + dependencies

--- api/main.py
+++ api/main.py
@@ -9,29 +9,21 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
 from starlette.responses import PlainTextResponse
 
-import mauve_api.mauve_db as mauve_db
-from mauve_api import models, exceptions
+from api import models, exceptions, mauve_db
+from api.model_descriptions import USER_EXAMPLE, PROJECT_EXAMPLE
 
 APP_LOGGER = logging.getLogger(__name__)
 USER_COLL_NAME = "users"
 PJ_COLL_NAME = "projects"
 
-app = FastAPI(title="MauveAPI", description="Mauve APP Backend", docs_url="/")
 
+app = FastAPI(
+    title="MauveAPI",
+    description="Mauve APP Backend",
+    docs_url="/",
+    redoc_url="/docs",
+)
 
-USER_EXAMPLE = {
-    "name": "Thanh Nguyen",
-    "email": "foo@bar.com",
-    "password": "<ENCRYPTED_JWT_TOKEN>"
-}
-
-PROJECT_EXAMPLE = {
-    "title": "Finding teammates!",
-    "user": "Thanh Nguyen",
-    "description": "Looking for teammates for the upcoming PearlHackathon",
-    "skills": ["opensource", "hackathon", "any"]
-
-}
 
 origins = [
     "https://pearlhacks2020-mauve.herokuapp.com/",
@@ -45,16 +37,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.add_middleware(HTTPSRedirectMiddleware)
+# app.add_middleware(HTTPSRedirectMiddleware)
 
 @app.on_event("startup")
-async def startup():
+def startup():
     APP_LOGGER.warning("Starting up app...")
     mauve_db.client_factory()
 
 
 @app.on_event("shutdown")
-async def shutdown():
+def shutdown():
     APP_LOGGER.warning("Shuting down app...")
     mauve_db.shutdown_client()
 
@@ -63,11 +55,13 @@ async def liveness():
     return {"status": "OK"}
 
 @app.get("/_db/{collection_name}/count")
-async def test_db(collection_name: str):
+def test_db(collection_name: str):
     return {"count": mauve_db.count(collection_name)}
 
 @app.post("/users", status_code=200)
-async def create_user(user_profile: models.User = Body(USER_EXAMPLE, example=USER_EXAMPLE)):
+def create_user(
+    ser_profile: models.User = Body(USER_EXAMPLE, example=USER_EXAMPLE)
+):
     """Create new users in users collection"""
     info = {"email" : user_profile.email}
     if mauve_db.count(USER_COLL_NAME, filter=info):
@@ -79,7 +73,7 @@ async def create_user(user_profile: models.User = Body(USER_EXAMPLE, example=USE
 
 
 @app.get("/users", response_model=models.Users)
-async def get_user_catalog():
+def get_user_catalog():
     """Get all users"""
     return {
         "users": [
@@ -89,7 +83,7 @@ async def get_user_catalog():
 
 
 @app.get("/users/{email}", response_model=models.User)
-async def get_user_by_email(email):
+def get_user_by_email(email):
     result = mauve_db.get_docs(USER_COLL_NAME, {"email" : email}, many=False)
     if not result:
         raise exceptions.NotFound
@@ -97,7 +91,7 @@ async def get_user_by_email(email):
 
 
 @app.get("/projects", response_model=models.Projects)
-async def project_catalog():
+def project_catalog():
     "Check the project catalog"
     return {
         "projects": [
@@ -107,7 +101,7 @@ async def project_catalog():
 
 
 @app.get("/projects", response_model=models.Projects)
-async def project_by_email(email: str):
+def project_by_email(email: str):
     "Check the project catalog"
     return {
         "projects": [
@@ -117,7 +111,9 @@ async def project_by_email(email: str):
 
 
 @app.post("/projects", status_code=200)
-async def create_project(project: models.Project = Body(PROJECT_EXAMPLE, example=PROJECT_EXAMPLE)) -> models.Projects:
+def create_project(
+    project: models.Project = Body(PROJECT_EXAMPLE, example=PROJECT_EXAMPLE)
+) -> models.Projects:
     "Post a project catalog"
     _id = mauve_db.insert_collection(PJ_COLL_NAME, docs=project.dict())
     mauve_db.update_collection(PJ_COLL_NAME, doc={"id": str(_id)}, filter={"_id": _id})
--- api/mauve_db.py
+++ api/mauve_db.py
@@ -1,5 +1,6 @@
 """
-MauveDB utils& managment
+mauve_api.mauve_db
+MauveDB utils & managment
 """
 from typing import Dict, List, Union
 from pprint import pformat as pf
@@ -32,13 +33,17 @@ def get_db() -> pymongo.database.Database:
         GLOBAL_MONGO.update(client=client)
     return client[MONGO_DB]
 
+
 def shutdown_client():
     """Shut down the mongo client"""
     MONGO_LOGGER.warning("Closing mongo client connection")
     client = GLOBAL_MONGO.pop("client", None)
-    if not client:
+    if client:
+        client.close()
+        MONGO_LOGGER.warning("Mongo client closed.")
+    else:
         MONGO_LOGGER.warning("No mongo client found. Do nothing...")
-    client.close()
+        
 
 def update_collection(col_name: str, doc: Dict, filter: Dict = None):
     """Update the collection given the collection_name, doc, and filter"""
@@ -46,11 +51,11 @@ def update_collection(col_name: str, doc: Dict, filter: Dict = None):
     collection = get_db()[col_name]
     return collection.update_many(filter, {"$set": doc})
 
+
 def insert_collection(col_name:str, docs: Union[Dict, List[Dict]]) -> Union[str, List[str]]:
     """Insert the collection with many or one doc"""
     MONGO_LOGGER.warning(f"Inserting {len(docs)} docs to {col_name}")
     collection = get_db()[col_name]
-    print(collection)
     inserted = None
     if isinstance(docs, list):
         inserted = collection.insert_many(docs).inserted_ids
@@ -58,6 +63,7 @@ def insert_collection(col_name:str, docs: Union[Dict, List[Dict]]) -> Union[str,
         inserted = collection.insert_one(docs).inserted_id
     MONGO_LOGGER.warning(f"Inserted doc(s) with id(s) {inserted}")
     return inserted
+
 
 def get_docs(col_name: str, filter: Dict = None, many=True) -> Union[Dict, List[Dict]]:
     """Get documents from given col_name collection"""
@@ -67,6 +73,7 @@ def get_docs(col_name: str, filter: Dict = None, many=True) -> Union[Dict, List[
         return collection.find(filter)
     else:
         return collection.find_one(filter)
+
 
 def count(col_name: str, filter: Dict = {}) -> int:
     """Get document counter"""
